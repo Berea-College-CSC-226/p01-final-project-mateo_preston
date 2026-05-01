8e7c230d23916afc2a175677e7e0cf27c554da29
Preston Worrix <worrixp@berea.edu>
2026-05-01 11:58:05 -0400

    cleaned up the folders and locations of files outlined gui class

---
+++ Game_Of_Nim_Files/Game_Of_Nim.py
@@ -0,0 +1,21 @@
+#Import
+
+class Gui:
+    def __init__(self):
+        pass
+
+    def single_or_Multi(self):
+        pass
+
+    def starting_marbles(self):
+        pass
+
+    # cant remeber what this was for in planning???
+    def draw_point(self):
+        pass
+
+    def take_marbles(self):
+        pass
+
+    def exit_game(self):
+        pass
