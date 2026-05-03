9e87b7b844b89a99e86067e3b4710a7c17bf4739
Preston Worrix <worrixp@berea.edu>
2026-05-03 16:35:14 -0400

    fixed format to be batter

--- Game_Of_Nim_Files/Game_Of_Nim.py
+++ Game_Of_Nim_Files/Game_Of_Nim.py
@@ -8,7 +8,10 @@ class App(tk.Tk):
         super().__init__()
         self.title("Game Of Nim")
         self.geometry("600x600")
-        self.minsize(600, 600)
+        self.minsize(400, 400)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
 
         #widgets
@@ -20,24 +23,29 @@ class App(tk.Tk):
 class Menu(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+        self.pack(expand=True, fill='both')
+
+        self.columnconfigure((0,1,2), weight=1)
+        self.rowconfigure((0,1,2,3,4), weight=1)
 
         self.create_widgets()
 
     def create_widgets(self):
-        start_btn = ttk.Button(self, text = "Start Game")
+        label = ttk.Label(self, text="Select Number of Items:", font=("Arial", 12))
+        label.grid(column=1, row=1, pady=10)
 
-        slider = tk.Scale(self, from_ = 15, to = 30, orient = tk.HORIZONTAL)
+        slider = ttk.LabeledScale(self, from_=15, to=30)
+        slider.grid(column=1, row=2, sticky="ew", padx=50)
 
-        self .columnconfigure(0, weight = 0, uniform = "a")
-        self.rowconfigure(0, weight = 0, uniform = "a")
-
-        start_btn.grid(column = 0, row = 1, sticky = "nsew")
-        slider.grid(column = 0, row = 0, sticky = "nsew")
+        start_btn = ttk.Button(self, text = "Start Game", command=self.start_game)
+        start_btn.grid(column=1, row=3, pady=20, ipadx=20, ipady=10)
 
 
-def main():
-    test = App()
+
+    def start_game(self):
+        print(f"Starting game with {self.item_count.get()} items!")
+
+
 
 if __name__ == "__main__":
-    main()
+    App()
