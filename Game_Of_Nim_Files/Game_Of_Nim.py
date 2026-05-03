fe365e4e47c642ec640068b0c35477cef69326df
Preston Worrix <worrixp@berea.edu>
2026-05-03 14:41:55 -0400

    started gui class got button test to work

--- Game_Of_Nim_Files/Game_Of_Nim.py
+++ Game_Of_Nim_Files/Game_Of_Nim.py
@@ -20,6 +20,20 @@ class App(tk.Tk):
 class Menu(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+        self.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        test = ttk.Button(self, text = "test")
+
+        entry = ttk.Entry(self)
+
+        self .columnconfigure((0,1,2), weight = 1, uniform = "a")
+        self.rowconfigure((0,1,2,3,4), weight = 1, uniform = "a")
+
+        test.grid(column = 0, row = 0, sticky = "nswe", columnspan = 2)
+
 
 def main():
     test = App()
